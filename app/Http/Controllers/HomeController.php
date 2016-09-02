edd98f749bce22a693a906375298f21e6b3b1f2e
infinitypaul <infinitypaul@live.com>
2016-09-02 08:10:20 +0100

    my changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,5 +8,9 @@ use App\Http\Requests;
 
 class HomeController extends Controller
 {
-    //
+public  function getIndex(){
+    return view('welcome');
+}
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::controller('/', 'HomeController');
+Route::get('/', ['as' => 'index', 'uses' => 'HomeController@getIndex']);
